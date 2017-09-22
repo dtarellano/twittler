35c570412e334d3b982b8389a85e956de9a9b323
David Arellano <dtarellano1@gmail.com>
2017-09-21 21:29:00 -0700

    every 4 sec a new tweet automatically is pushed into the feed

--- script.js
+++ script.js
@@ -6,9 +6,26 @@ $(document).ready(function(){
     while(index >= 0){
       var tweet = streams.home[index];
       var $tweet = $('<li class="list-group-item"></li>');
-        $tweet.text('@' + tweet.user + ': ' + tweet.message);
+        $tweet.text(`@${tweet.user}: ${tweet.message} posted at ${tweet.created_at}`)
         $tweet.appendTo('.list-group');
         index -= 1;
     }
-//<li class="list-group-item">Vestibulum at eros</li>
+
+    var checkForTweets = function() {
+      setInterval(function() {
+        var newTweetCheck = streams.home.length - 1
+        if (newTweetCheck > index) {
+          $('.list-group-item').remove();
+          while(newTweetCheck >= 0) {
+            var tweet = streams.home[newTweetCheck];
+            var $tweet = $('<li class="list-group-item"></li>');
+              $tweet.text(`@${tweet.user}: ${tweet.message} posted at ${tweet.created_at}`)
+              $tweet.appendTo('.list-group');
+              newTweetCheck -= 1;
+          }
+        }
+      }, 3000)
+    }
+
+    checkForTweets()
 });
