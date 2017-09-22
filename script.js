202a086e7b1dd8082b0c0311920aca9b55165356
David Arellano <dtarellano1@gmail.com>
2017-09-21 20:08:57 -0700

    added bootstrap and made feed append to list class

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
   var $body = $('body');
-  $body.html('');
+  //$body.html('');
 
     var index = streams.home.length - 1;
     while(index >= 0){
       var tweet = streams.home[index];
-      var $tweet = $('<div></div>');
+      var $tweet = $('<li class="list-group-item"></li>');
         $tweet.text('@' + tweet.user + ': ' + tweet.message);
-        $tweet.appendTo($body);
+        $tweet.appendTo('.list-group');
         index -= 1;
     }
-
+//<li class="list-group-item">Vestibulum at eros</li>
 });
