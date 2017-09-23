2ad5e67dc8caa03f6ff6349804a7ac051ea6d7ec
David Arellano <dtarellano1@gmail.com>
2017-09-22 17:35:47 -0700

    added notification bar and on click produces all new and old tweets

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
+
 $(document).ready(function(){
   var $body = $('body');
   //$body.html('');
+  var index = streams.home.length - 1;
+    var streamTweets = function() {
 
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      var $tweet = $('<li class="list-group-item"></li>');
-        $tweet.text(`@${tweet.user}: ${tweet.message} posted at ${tweet.created_at}`)
-        $tweet.appendTo('.list-group');
-        index -= 1;
+      var loop = streams.home.length - 1;
+
+      while (loop >= 0) {
+        var tweet = streams.home[loop];
+        var $tweet = $('<li class="list-group-item tweet"></li>');
+          $tweet.text(`@${tweet.user}: ${tweet.message} posted at ${tweet.created_at}`)
+          $tweet.appendTo('.news-feed');
+          loop -= 1;
+      }
+    index = streams.home.length; - 1;
     }
 
-    var checkForTweets = function() {
-      setInterval(function() {
-        var newTweetCheck = streams.home.length - 1
+
+    var checkForTweets;
+
+    var tweetTimer = function() {
+    checkForTweets = setInterval(function() {
+      var newTweetCheck = streams.home.length - 1
+
         if (newTweetCheck > index) {
-          $('.list-group-item').remove();
-          while(newTweetCheck >= 0) {
-            var tweet = streams.home[newTweetCheck];
-            var $tweet = $('<li class="list-group-item"></li>');
-              $tweet.text(`@${tweet.user}: ${tweet.message} posted at ${tweet.created_at}`)
-              $tweet.appendTo('.list-group');
-              newTweetCheck -= 1;
-          }
+          $('#notification').text(`You have ${newTweetCheck - index} new Tweets`)
+          $('#notification').slideDown('slow')
         }
-      }, 3000)
+      }, 4000);
     }
 
-    checkForTweets()
+    streamTweets()
+    tweetTimer()
+
+    $('#notification').on('click', function() {
+      clearInterval(checkForTweets)
+      $('.tweet').remove();
+      streamTweets()
+      $('#notification').hide();
+      tweetTimer()
+    })
+
 });
