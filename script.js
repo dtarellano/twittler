e2e5620a4f6cbe686c0be627f12410333eee9b1b
David Arellano <dtarellano1@gmail.com>
2017-09-23 11:25:41 -0700

    clicking user gives feed of that particular user

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
 
 $(document).ready(function(){
-  var $body = $('body');
-  //$body.html('');
   var index = streams.home.length - 1;
-  
-    var streamTweets = function() {
-      var loop = streams.home.length - 1;
+
+
+    var streamTweets = function(user) {
+      var stream;
+
+      if (!arguments) {
+        stream = streams.users[user];
+      } else {
+        stream = streams.home;
+      }
+      var loop = stream.length - 1;
 
       while (loop >= 0) {
-        var tweet = streams.home[loop];
-        var $tweet = $(`<li class="list-group-item tweet"><a href="#">@${tweet.user}</a><p>${tweet.message}</p><strong>${tweet.created_at}</strong</li>`);
+        var tweet = stream[loop];
+        var $tweet = $(`<li class="list-group-item tweet"><a href="#" data-user="${tweet.user}">@${tweet.user}</a><p>${tweet.message}</p><strong>${tweet.created_at}</strong</li>`);
           $tweet.appendTo('.news-feed');
           loop -= 1;
       }
 
       index = streams.home.length; - 1;
-    }
+    };
 
 
     var checkForTweets;
 
     var tweetTimer = function() {
     checkForTweets = setInterval(function() {
-      var newTweetCheck = streams.home.length - 1
+      var newTweetCheck = streams.home.length - 1;
 
         if (newTweetCheck > index) {
           $('#notification').text(`You have ${newTweetCheck - index} new Tweets`)
           $('#notification').slideDown('slow')
         }
-      }, 4000);
-    }
+      }, 5000);
+    };
 
     streamTweets()
     tweetTimer()
@@ -40,6 +46,13 @@ $(document).ready(function(){
       streamTweets()
       $('#notification').hide();
       tweetTimer()
-    })
+    });
 
+    $('.tweet').on('click', 'a', function() {
+      var $user = $(this).data('user');
+      clearInterval(checkForTweets)
+      $('.tweet').remove();
+      streamTweets($user)
+      $('#notification').hide();
+    });
 });
